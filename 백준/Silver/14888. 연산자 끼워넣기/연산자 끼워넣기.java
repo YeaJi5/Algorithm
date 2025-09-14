3177ca2d67e2380adb3e41c73c6a9f4288376609
YEAJI <166423163+YeaJi5@users.noreply.github.com>
2025-09-14 13:54:29 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 116 ms, Memory: 16768 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int[] numList;
+    static int[] operCnt;
+    static int minV = Integer.MAX_VALUE;
+    static int maxV = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        numList = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=0; i < N; i++){
+            numList[i] = Integer.parseInt(st.nextToken());
+        }
+
+        operCnt = new int[4];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 4; i++){
+            operCnt[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(1, numList[0]);
+
+        System.out.println(maxV);
+        System.out.println(minV);
+
+    }
+
+    static void dfs(int idx, int result){
+        if (idx == N){
+            minV = Math.min(minV, result);
+            maxV = Math.max(maxV, result);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++){
+            if (operCnt[i] > 0) {
+                operCnt[i]--;
+
+                if (i == 0){
+                    dfs(idx+1, result+numList[idx]);
+                }
+                else if (i == 1){
+                    dfs(idx+1, result-numList[idx]);
+                }
+                else if (i == 2){
+                    dfs(idx+1, result*numList[idx]);
+                }
+                else {
+                    if (result < 0){
+                        dfs(idx+1, -(-result / numList[idx]));
+                    }else{
+                        dfs(idx+1, result/numList[idx]);
+                    }
+                }
+
+                operCnt[i]++;
+            }
+        }
+    }
+}
