8998bbff83ed36069bddb4545de858ae417676ca
YEAJI <166423163+YeaJi5@users.noreply.github.com>
2025-09-24 21:41:44 +0900

    [Bronze IV] Title: 주사위 세개, Time: 100 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int dice1 = Integer.parseInt(st.nextToken());
+        int dice2 = Integer.parseInt(st.nextToken());
+        int dice3 = Integer.parseInt(st.nextToken());
+        int score = 0;
+
+        if (dice1 == dice2 && dice2 == dice3) {
+            score = (10000 + dice1 * 1000);
+        }
+        else if (dice1 == dice2) {
+            score = 1000 + dice1 * 100;
+        }
+        else if (dice2 == dice3){
+            score = 1000 + dice2 * 100;
+        }
+        else if (dice1 == dice3) {
+            score = 1000 + dice1 * 100;
+        }
+        else{
+            int maxDice = Math.max(dice1, Math.max(dice2, dice3));
+            score = maxDice * 100;
+            }
+
+        System.out.println(score);
+
+    }
+}
