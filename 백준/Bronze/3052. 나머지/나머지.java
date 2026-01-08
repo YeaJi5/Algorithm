222cb326e123b1b27086146f11465f61be81f59d
YEAJI <166423163+YeaJi5@users.noreply.github.com>
2026-01-08 22:43:26 +0900

    [Bronze II] Title: 나머지, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Set<Integer> s = new HashSet<>();
+
+        for (int i=0; i<10; i++){
+            int num = Integer.parseInt(br.readLine());
+
+            int remain = num % 42;
+            s.add(remain);
+        }
+
+        System.out.println(s.size());
+    }
+}
