604f2cfdc36227a441fed94d560d61f5987b8e19
YEAJI <166423163+YeaJi5@users.noreply.github.com>
2026-01-22 22:25:14 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i=1; i<=num; i++){
+            for(int j=num; j>0; j--){
+                if (i>=j){
+                    sb.append('*');
+                } else{
+                    sb.append(" ");
+                }
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
