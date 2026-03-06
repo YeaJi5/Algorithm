ddbb67276743a571096848473fabc8bf1d3c814c
YEAJI <166423163+YeaJi5@users.noreply.github.com>
2026-03-06 22:43:50 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+    
+        // n 최대값 11
+        int[] dp = new int[12];
+
+        dp[1] = 1;   // 1
+        dp[2] = 2;   // 1+1, 2
+        dp[3] = 4;   // 1+1+1, 1+2, 2+1, 3
+
+        for (int i = 4; i <= 11; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        for (int tc = 0; tc < T; tc++) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[n]).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
