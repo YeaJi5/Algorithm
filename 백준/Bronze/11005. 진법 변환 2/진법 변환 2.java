76ea1227aa8a5d8eedf4ba701a4623882e9353e3
YEAJI <166423163+YeaJi5@users.noreply.github.com>
2026-03-26 22:38:59 +0900

    [Bronze I] Title: 진법 변환 2, Time: 116 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+
+        while (N > 0) {
+            int remainder = N % B;
+
+            if (remainder < 10) {
+                sb.append(remainder);
+            } else {
+                sb.append((char)('A' + (remainder - 10)));
+            }
+
+            N /= B;
+        }
+
+        System.out.println(sb.reverse());
+    }
+}
